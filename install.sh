98ae3467b3d433abc2ff1607428b87806620e6c2
Matt Jones <gitcode@magisa.org>
2011-02-08 10:33:40 +0000

    Comment out broken build.

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 pwd
-# aptitude install r-recommended
+# aptitude install r-recommended r-cran-rjava
+# R CMD javareconf
+# R> install.packages("rJava")
 R --version
-R CMD INSTALL d1_client_r
+#R CMD INSTALL d1_client_r
+
+# The following NEW packages will be installed:
+  #ca-certificates-java{a} hicolor-icon-theme{a} icedtea-6-jre-cacao{a} 
+  #libaccess-bridge-java{a} libaccess-bridge-java-jni{a} libatk1.0-0{a} 
+  #libatk1.0-data{a} libavahi-client3{a} libcups2{a} libflac8{a} libgif4{a} 
+  #libgtk2.0-0{a} libgtk2.0-bin{a} libgtk2.0-common{a} libjasper1{a} 
+  #libnspr4-0d{a} libnss3-1d{a} libogg0{a} libpulse0{a} libsndfile1{a} 
+  #libvorbis0a{a} libvorbisenc2{a} libxcomposite1{a} libxcursor1{a} 
+  #openjdk-6-jre{a} openjdk-6-jre-headless{a} openjdk-6-jre-lib{a} r-cran-rjava 
+  #ttf-dejavu-core{a} tzdata-java{a} 
+
