bf441428471d7f810b1edf674cd7a409f83728cd
Matt Jones <gitcode@magisa.org>
2011-02-08 20:59:41 +0000

    iUpdate installation script for R hudson build.

--- install.sh
+++ install.sh
@@ -1,20 +1,12 @@
 #!/bin/sh
-pwd
+#
+# A shell script that installs and executes the D1 R Client code.
+#
 # aptitude install r-recommended r-cran-rjava
 # R CMD javareconf
 # R> install.packages("rJava")
 # ln -s /usr/local/lib/R/site-library/rJava/jri/libjri.so /usr/lib
 R --version
-source /etc/R/ldpaths
+#source /etc/R/ldpaths
 R CMD INSTALL d1_client_r
-
-# The following NEW packages will be installed:
-  #ca-certificates-java{a} hicolor-icon-theme{a} icedtea-6-jre-cacao{a} 
-  #libaccess-bridge-java{a} libaccess-bridge-java-jni{a} libatk1.0-0{a} 
-  #libatk1.0-data{a} libavahi-client3{a} libcups2{a} libflac8{a} libgif4{a} 
-  #libgtk2.0-0{a} libgtk2.0-bin{a} libgtk2.0-common{a} libjasper1{a} 
-  #libnspr4-0d{a} libnss3-1d{a} libogg0{a} libpulse0{a} libsndfile1{a} 
-  #libvorbis0a{a} libvorbisenc2{a} libxcomposite1{a} libxcursor1{a} 
-  #openjdk-6-jre{a} openjdk-6-jre-headless{a} openjdk-6-jre-lib{a} r-cran-rjava 
-  #ttf-dejavu-core{a} tzdata-java{a} 
-
+R --no-save -e 'library(dataone); d1.t1();'
