be45a9c27ad3ca89dc8b138f65f0a460b2e3c1f7
Andy Pippin <pippin@nceas.ucsb.edu>
2012-08-07 16:51:20 +0000

    getD1Object() now returns a Java org.dataone.client.D1Object. Massive delta due to moving from a 3-character indent to a 4 character. Work still needs to be done on getPackage().

--- R/D1Client-methods.R
+++ R/D1Client-methods.R
@@ -35,48 +35,48 @@ setGeneric("getPackage", function(x, identifier, ...) {
 })
 
 setMethod("getPackage", "D1Client", function(x, identifier) {
-   client <- x@client
-   session <- x@session
-   pid <- .jnew("org/dataone/service/types/v1/Identifier")
-   pid$setValue(identifier)
+    client <- x@client
+    session <- x@session
+    pid <- .jnew("org/dataone/service/types/v1/Identifier")
+    pid$setValue(identifier)
 
-   # Resolve the ID to find the MN to use
-   cnode <- client$getCN()
-   print("Trying resolve operation....")
-   oll <- cnode$resolve(session, pid)
-   olist <- oll$getObjectLocationList()
-   objloc <- olist$get(as.integer(0))
-   node <- objloc$getNodeIdentifier()
-   #nodeid <- node$getValue()
-   #print(nodeid)
-   nodeurl <- objloc$getBaseURL()
-   print(nodeurl)
+    # Resolve the ID to find the MN to use
+    cnode <- client$getCN()
+    print("Trying resolve operation....")
+    oll <- cnode$resolve(session, pid)
+    olist <- oll$getObjectLocationList()
+    objloc <- olist$get(as.integer(0))
+    node <- objloc$getNodeIdentifier()
+    nodeid <- node$getValue()
+    #print(nodeid)
+    nodeurl <- objloc$getBaseURL()
+    print(nodeurl)
 
-   # Get the sysmeta for this GUID to decide how to handle it
-   print("Trying sysmeta operation....")
-   sysmeta <- cnode$getSystemMetadata(session, pid)
-   .jcheck(silent = FALSE)
-   fmtid <- sysmeta$getFormatId()
-   oformat = cnode$getFormat(fmtid)
-   #print(oformat$toString())
+    # Get the sysmeta for this GUID to decide how to handle it
+    print("Trying sysmeta operation....")
+    sysmeta <- cnode$getSystemMetadata(session, pid)
+    .jcheck(silent = FALSE)
+    fmtid <- sysmeta$getFormatId()
+    oformat = cnode$getFormat(fmtid)
+    #print(oformat$toString())
 
-   # Now get the object from the correct MN
-   print("Trying read operation....")
-   mnode <- client$getMN(nodeurl)
-   datastream <- mnode$get(session, pid) 
-   .jcheck(silent = FALSE)
-   ioUtils <-  .jnew("org/apache/commons/io/IOUtils") 
-   .jcheck(silent = FALSE)
-   rdata <- ioUtils$toString(datastream)
+    # Now get the object from the correct MN
+    print("Trying read operation....")
+    mnode <- client$getMN(nodeurl)
+    datastream <- mnode$get(session, pid) 
+    .jcheck(silent = FALSE)
+    ioUtils <-  .jnew("org/apache/commons/io/IOUtils") 
+    .jcheck(silent = FALSE)
+    rdata <- ioUtils$toString(datastream)
 
-   # Load the data into a dataframe
-   df <- read.table(textConnection(rdata), header = TRUE, sep = ",", na.strings = "-999")
-   #df <- read.table(textConnection(rdata), header = FALSE, skip=27)
+    # Load the data into a dataframe
+    df <- read.table(textConnection(rdata), header = TRUE, sep = ",", na.strings = "-999")
+    #df <- read.table(textConnection(rdata), header = FALSE, skip=27)
 
-   scimeta <- "Placeholder string for science metadata, waiting to implement lookup of sci metadata from describedBy field in sysmeta"
-   dp <- DataPackage(identifier, sysmeta, scimeta)
-   dp <- addData(dp, df)
-   return(dp)
+    scimeta <- "Placeholder string for science metadata, waiting to implement lookup of sci metadata from describedBy field in sysmeta"
+    dp <- DataPackage(identifier, sysmeta, scimeta)
+    dp <- addData(dp, df)
+    return(dp)
 })
 
 
@@ -86,28 +86,77 @@ setGeneric("getD1Object", function(x, identifier, ...) {
 })
 
 setMethod("getD1Object", "D1Client", function(x, identifier) {
-   client <- x@client
-   session <- x@session
-   pid <- .jnew("org/dataone/service/types/v1/Identifier")
-   pid$setValue(identifier)
+    client <- x@client
+    session <- x@session
+    pid <- .jnew("org/dataone/service/types/v1/Identifier")
+    pid$setValue(identifier)
 
-   # Use libclient D1Object to get bytes of object
-   #print("Trying to get the object ...")
-   d1obj <- J("org/dataone/client/D1Object")$download(pid)
-   databytes <- d1obj$getData() 
-   .jcheck(silent = FALSE)
+    # Use libclient D1Object to get bytes of object
+    # First, find a member node
+    cn <- client$getCN()
+    oll <- cn$resolve(session, pid)
+    if (!is.null(e<-.jgetEx())) {
+	print("Java exception was raised")
+	print(.jcheck(silent=TRUE))
+    }
+    olist <- oll$getObjectLocationList()
+    objloc <- olist$get(as.integer(0))
+    node <- objloc$getNodeIdentifier()
+    nodeid <- node$getValue()
 
-   #print("Convert to string...")
-   jString <-  .jnew("java/lang/String", databytes) 
-   .jcheck(silent = FALSE)
-   rdata <- jString$toString()
+    # Second, get the object.
+    d1Object <- J("org/dataone/client/D1Object")$download(pid)
+    .jcheck(silent = FALSE)
 
-   #print("Pull out sysmeta...")
-   sysmeta <- d1obj$getSystemMetadata()
-   scimeta <- "Placeholder string for science metadata, waiting to implement lookup of sci metadata from describedBy field in sysmeta"
-   dp <- DataPackage(identifier, sysmeta, scimeta)
-   dp <- addData(dp, rdata)
-   return(dp)
+    return(d1Object)
+})
+
+
+## create
+setGeneric("create", function(x, J_d1Object, ...) { 
+    standardGeneric("create")
+})
+
+setMethod("create", "D1Client", function(x, J_d1Object) {
+    # -- Validate everything necessary to create new object..
+    if(is.jnull(J_d1Object)) {
+        print("Cannot create a null object.")
+	return(FALSE)
+    }
+    if(is.jnull(J_d1Object$getSystemMetadata())) {
+        print("Cannot create with a null sysmeta object.")
+	return(FALSE)
+    }
+print("getSystemMetaData")
+    sysmeta <- J_d1Object$getSystemMetadata()
+    if (!is.null(e<-.jgetEx())) {
+	print("Java exception was raised")
+	print(.jcheck(silent=TRUE))
+    }
+
+    if(is.jnull(sysmeta$getIdentifier())) {
+        print("Cannot create with a null identifier.")
+	return(FALSE)
+    }
+    pid <- sysmeta$getIdentifier()
+
+    cn <- getCN(x)
+    oll <- cn$resolve(x@session, pid)
+    if(!is.jnull(d1Client@mnRef)) {
+        print(paste(pid, "- object already exists."))
+	return(FALSE)
+    }
+
+    if(is.jnull(d1Client@mnRef)) {
+        print("Cannot create an object without a Member Node defined.")
+	return(FALSE)
+    }
+
+    object <- .jnew("java/io/ByteArrayInputStream", J_d1Object$getData())
+    mn <- getMN(x)
+    newPid <- mn$create(x@session, pid, object, sysmeta)
+
+    return(is.jnull(newPid))
 })
 
 
@@ -144,6 +193,28 @@ setMethod("setMNodeId", "D1Client", function(x, id) {
 	x@mnRef <- nodeRef
     }
 })
+
+setGeneric("getMN", function(x, ...) { 
+    standardGeneric("getMN")
+})
+setMethod("getMN", "D1Client", function(x) {
+    if(is.jnull(x@mnRef)) {
+        e <- .jnew("org/dataone/service/exceptions/ServiceFailure", "1102", "Invalid Request: No member node defined.")
+	.jthrow(e)
+    }
+
+    mn <- J("org/dataone/client/D1Client")$getMN(x@mnRef)
+    return(mn)
+})
+
+setGeneric("getCN", function(x, ...) { 
+    standardGeneric("getCN")
+})
+setMethod("getCN", "D1Client", function(x) {
+    cn <- J("org/dataone/client/D1Client")$getCN()
+    return(cn)
+})
+
 
 #########################################################
 ### Utility methods
