261130f87ab2bf806bd2202b11e7a9c7a48cfa71
Chris Jones <csj@christopherjones.co>
2013-02-25 03:11:43 +0000

    Add in the .onLoad() method as a hook function that gets called when the package is loaded.  It basically just calls .jpackage() to add Java libraries to the classpath.

---
+++ dataonelibs/R/onLoad.R
@@ -0,0 +1,24 @@
+#
+#   This work was created by participants in the DataONE project, and is
+#   jointly copyrighted by participating institutions in DataONE. For
+#   more information on DataONE, see our web site at http://dataone.org.
+#
+#     Copyright 2011-2013
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
+.onLoad <- function(libname, pkgname) {
+  .jpackage(pkgname, jars='*', lib.loc = libname)
+}
+
