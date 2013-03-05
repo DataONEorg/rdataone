52c1c894618f95b1d646073d9ee52bea9c6e9b69
Rob Nahf <rnahf@epscor.unm.edu>
2013-03-05 23:26:57 +0000

    start of RUnit testing structure for dataone R extension.

---
+++ dataone/inst/test/runIntegrationTests.R
@@ -0,0 +1,27 @@
+
+##
+##  Runs all of the tests in the test/unit directory,
+##  trying first to find the tests in the project directory, then
+##  trying to locate in the R installed packages library
+##
+
+library(dataone)
+library(RUnit)
+
+testDir <- function() {
+    curDir <- Sys.getenv("PWD")
+    if (grepl("d1_client_r",curDir)) {
+        ## in the project directory, so have the path
+        testDir <- sub("d1_client_r.+","d1_client_r/dataone/inst/test/integration",curDir)
+    } else {
+        testDir <- file.path(.path.package(package="dataone"),"inst/test/integration")
+    }
+    return(testDir)
+}
+
+theTestSuite <- defineTestSuite("integrationTests",
+        dirs = testDir(),
+        testFileRegexp = "^runit.+\\.R")
+
+testResult <- runTestSuite(theTestSuite)
+printTextProtocol(testResult)
