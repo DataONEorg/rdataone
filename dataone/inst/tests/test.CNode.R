1ce30d20a98b8041cbb417fd45f3d63c48e1a893
Matt Jones <gitcode@magisa.org>
2013-11-15 23:45:27 -0900

    Initial infrastructure for writing 'testthat' tests.  To run, load library(devtools), and then run test().  See testthat for more options on how to run tests. Tests go in inst/tests.

---
+++ dataone/inst/tests/test.CNode.R
@@ -0,0 +1,11 @@
+context("CNode tests")
+test_that("dataone library loads", {
+	library(dataone)
+})
+test_that("CNode constructors work", {
+	library(dataone)
+	cn <- CNode()
+	expect_that(cn@endpoint, matches("https://cn.dataone.org/cn"))
+	cn <- CNode("DEV")
+	expect_that(cn@endpoint, matches("https://cn-dev.test.dataone.org/cn"))
+})
