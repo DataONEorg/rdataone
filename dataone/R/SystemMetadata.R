5b745136dd62d59c84f6d8738e0be26f1be613f3
Jordan S Read <jread@usgs.gov>
2014-09-02 17:32:02 -0500

    copyright to 2014

--- dataone/R/SystemMetadata.R
+++ dataone/R/SystemMetadata.R
@@ -1,4 +1,52 @@
 
+## TODO: method to parse SystemMetadata from XML
+#
+#   This work was created by participants in the DataONE project, and is
+#   jointly copyrighted by participating institutions in DataONE. For
+#   more information on DataONE, see our web site at http://dataone.org.
+#
+#     Copyright 2011-2014
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#'@description A class representing DataONE SystemMetadata, which is core information about objects stored in a repository
+#' and needed to manage those objects across systems.  SystemMetadata contains basic identification, ownership,
+#' access policy, replication policy, and related metadata.
+#'
+#'@slot serialVersion value of type \code{"numeric"}, the current version of this system metadata; only update the current version
+#'@slot identifier value of type \code{"character"}, the identifier of the object that this system metadata describes.
+#'@slot replicationAllowed value of type \code{"logical"}, for replication policy allows replicants.
+#'@slot numberReplicas value of type \code{"numeric"}, for number of supported replicas.
+#'@slot preferredNodes value of type \code{"list"}, of prefered member nodes.
+#'@slot blockedNodes value of type \code{"list"}, of blocked member nodes.
+#'@slot formatId value of type \code{"character"}, the DataONE object format for the object.
+#'@slot size value of type \code{"numeric"}, the size of the object in bytes.
+#'@slot checksum value of type \code{"character"}, the checksum for the object using the designated checksum algorithm.
+#'@slot checksumAlgorithm value of type \code{"character"}, the name of the hash function used to generate a checksum, from the DataONE controlled list.
+#'@slot submitter value of type \code{"character"}, the Distinguished Name or identifier of the person submitting the object.
+#'@slot rightsHolder value of type \code{"character"}, the Distinguished Name or identifier of the person who holds access rights to the object.
+#'@slot accessPolicy value of type \code{"data.frame"}, a list of access rules to be applied to the object.
+#'@slot obsoletes value of type \code{"character"}, the identifier of an object which this object replaces.
+#'@slot obsoletedBy value of type \code{"character"}, the identifier of an object that replaces this object.
+#'@slot archived value of type \code{"logical}, a boolean flag indicating whether the object has been archived and thus hidden.
+#'@slot dateUploaded value of type \code{"character"}, the date on which the object was uploaded to a member node.
+#'@slot dateSysMetadataModified value of type \code{"character"}, the last date on which this system metadata was modified.
+#'@slot originMemberNode value of type \code{"character"}, the node identifier of the node on which the object was originally registered.
+#'@slot authoritativeMemberNode value of type \code{"character"}, the node identifier of the node which currently is authoritative for the object.
+#'
+#' @return the SystemMetadata object representing an object
+#' @author jones
+#' @export
 setClass("SystemMetadata", slots = c(
     serialVersion           = "numeric",
     identifier              = "character",
@@ -24,38 +72,6 @@ setClass("SystemMetadata", slots = c(
     ), )
 
 
-#'@description A class representing DataONE SystemMetadata, which is core information about objects stored in a repository
-#' and needed to manage those objects across systems.  SystemMetadata contains basic identification, ownership,
-#' access policy, replication policy, and related metadata.
-#'
-#'@section Slots: 
-#'  \describe{
-#'    \item{\code{serialVersion}:}{value of type \code{"numeric"}, the current version of this system metadata; only update the current version}
-#'    \item{\code{identifier}:}{value of type \code{"character"}, the identifier of the object that this system metadata describes.}
-#'    \item{\code{replicationAllowed}:}{value of type \code{"logical"}, for replication policy allows replicants.}
-#'    \item{\code{numberReplicas}:}{value of type \code{"numeric"}, for number of supported replicas.}
-#'    \item{\code{preferredNodes}:}{value of type \code{"list"}, of prefered member nodes.}
-#'    \item{\code{blockedNodes}:}{value of type \code{"list"}, of blocked member nodes.}
-#'    \item{\code{formatId}:}{value of type \code{"character"}, the DataONE object format for the object.}
-#'    \item{\code{size}:}{value of type \code{"numeric"}, the size of the object in bytes.}
-#'    \item{\code{checksum}:}{value of type \code{"character"}, the checksum for the object using the designated checksum algorithm.}
-#'    \item{\code{checksumAlgorithm}:}{value of type \code{"character"}, the name of the hash function used to generate a checksum, from the DataONE controlled list.}
-#'    \item{\code{submitter}:}{value of type \code{"character"}, the Distinguished Name or identifier of the person submitting the object.}
-#'    \item{\code{rightsHolder}:}{value of type \code{"character"}, the Distinguished Name or identifier of the person who holds access rights to the object.}
-#'    \item{\code{accessPolicy}:}{value of type \code{"data.frame"}, a list of access rules to be applied to the object.}
-#'    \item{\code{obsoletes}:}{value of type \code{"character"}, the identifier of an object which this object replaces.}
-#'    \item{\code{obsoletedBy}:}{value of type \code{"character"}, the identifier of an object that replaces this object.}
-#'    \item{\code{archived}:}{value of type \code{"logical}, a boolean flag indicating whether the object has been archived and thus hidden.}
-#'    \item{\code{dateUploaded}:}{value of type \code{"character"}, the date on which the object was uploaded to a member node.}
-#'    \item{\code{dateSysMetadataModified}:}{value of type \code{"character"}, the last date on which this system metadata was modified.}
-#'    \item{\code{originMemberNode}:}{value of type \code{"character"}, the node identifier of the node on which the object was originally registered.}
-#'    \item{\code{authoritativeMemberNode}:}{value of type \code{"character"}, the node identifier of the node which currently is authoritative for the object.}
-#'  }
-#'
-#' Construct SystemMetadata, with all fields as null
-#' @return the SystemMetadata object representing an object
-#' @author jones
-## @export
 setGeneric(name = "SystemMetadata", def = function(x) {
   standardGeneric("SystemMetadata")
 })
@@ -90,16 +106,14 @@ setMethod("SystemMetadata", signature(), function(x) {
 ## Methods
 ##########################
 
-## TODO: method to parse SystemMetadata from XML
-
-
 #' @description
 #' Parse an XML representation of system metadata, and set the object slots with obtained values.
 #' @param sysmeta the \code{SystemMetadata} object
 #' @param xml the XML representation of the capabilities, as an XMLInternalElementNode
-#' @param ... additional arguments passed to ??? (where are these passed to?)
+#' @param ... additional arguments passed to other functions or methods
 #' @return the SystemMetadata object representing an object
 #' 
+#' @docType methods
 #' @author jones
 #' @export
 setGeneric("parseSystemMetadata", function(sysmeta, xml, ...) {
@@ -179,6 +193,7 @@ setMethod("parseSystemMetadata", signature("SystemMetadata", "XMLInternalElement
 #' @param sysmeta the SystemMetadata instance to be serialized
 #' @return the character string representing a SystemMetadata object
 #' @author jones 
+#' @docType methods
 #' @export
 setGeneric("serialize", function(sysmeta, ...) {
     standardGeneric("serialize")
