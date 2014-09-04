1105c0a8bf87bfdf271a145ce73456987c2fd176
Ted Hart <edmund.m.hart@gmail.com>
2014-09-04 09:41:28 -0700

    - Added list format functionality - prettified output of resolve to match list format output - added test for list format - modified resolve test

--- dataone/R/CNode.R
+++ dataone/R/CNode.R
@@ -93,8 +93,43 @@ setMethod("CNode", signature("character"), function(env) {
 # @see http://mule1.dataone.org/ArchitectureDocs-current/apis/CN_APIs.html#CN_core.ping
 # public Date ping()
 
-# @see http://mule1.dataone.org/ArchitectureDocs-current/apis/CN_APIs.html#CNCore.listFormats
-# public ObjectFormatList listFormats()
+
+#' list formats
+#' @description list of all object formats registered in the DataONE Object Format Vocabulary.
+#' @param cnode a valid CNode object
+#' @docType methods
+#' @author hart
+#' @return Returns a dataframe of all object formats registered in the DataONE Object Format Vocabulary.
+#' @example /dontrun{
+#' cn <- CNode()
+#' listFormats(cn)
+#' }
+#' @export
+
+
+setGeneric("listFormats", function(cnode, ...) {
+  standardGeneric("listFormats")
+})
+
+
+#' @rdname listFormats-method
+#' @aliases listFormats
+#' @export
+
+
+setMethod("listFormats", signature("CNode"), function(cnode){
+  url <- paste(cnode@endpoint,"formats",sep="/")
+  out <- GET(url)
+  out <- xmlToList(content(out,as="parsed"))
+  ## Below could be done with plyr functionality, but I want to reduce
+  ## dependencies in the package
+  df <- data.frame(matrix(NA,ncol=3,nrow=(length(out)-1)))
+  for(i in 1:(length(out)-1)){
+    df[i,] <- c(unlist(out[[i]]))
+  }
+  colnames(df) <- c("ID","Name","Type")
+  return(df)
+})
 
 # @see http://mule1.dataone.org/ArchitectureDocs-current/apis/CN_APIs.html#CNCore.getFormat
 # public ObjectFormat getFormat(ObjectFormatIdentifier formatid)
@@ -181,11 +216,20 @@ setGeneric("resolve", function(cnode,pid) {
 
 #' @rdname resolve-method
 #' @aliases resolve
+#' @export
 
 setMethod("resolve", signature("CNode" ,"character"), function(cnode,pid){
   url <- paste(cnode@endpoint,"resolve",pid,sep="/")
   out <- GET(url,add_headers(Accept = "text/xml"),config=config(followlocation = 0L))
-  return(xmlToList(content(out,as="parsed")))
+  out <- xmlToList(content(out,as="parsed"))
+  # Using a loop when plyr would work to reduce dependencies.
+  df <- data.frame(matrix(NA,ncol=4,nrow=(length(out)-1)))
+  for(i in 2:length(out)){
+    df[(i-1),] <- c(unlist(out[[i]]))
+  }
+  colnames(df) <- c("identifier","baseURL","version","url")
+  toret <- list(id = pid, data = df)
+  return(toret)
 })
     
 # @see http://mule1.dataone.org/ArchitectureDocs-current/apis/CN_APIs.html#CNRead.getChecksum
--- dataone/inst/tests/test.CNode.R
+++ dataone/inst/tests/test.CNode.R
@@ -44,5 +44,12 @@ test_that("CNode resolve()",{
   cn <- CNode()
   id <- "0d7d8e0e-93f5-40ab-9916-501d7cf93e15"
   res <- resolve(cn,id)
-  expect_that(res$identifier, matches(id) )
+  expect_that(res$id, matches(id) )
+  expect_that(typeof(res$data),matches("list") )
+})
+test_that("CNode listFormats",{
+  library(dataone) 
+  cn <- CNode()
+  f <- listFormats(cn)
+  expect_that(is.data.frame(f),is_true())
 })
