566457e9360a08a32b3eeb136fb45b287092ff61
Matt Jones <gitcode@magisa.org>
2014-10-03 19:33:52 -0800

    Updated roxygen docs in CertificateManager.

--- dataone/R/CertificateManager.R
+++ dataone/R/CertificateManager.R
@@ -3,7 +3,7 @@
 #   jointly copyrighted by participating institutions in DataONE. For
 #   more information on DataONE, see our web site at http://dataone.org.
 #
-#     Copyright 2011-2013
+#     Copyright 2012-2014
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -17,16 +17,75 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 #
-library(stringr)
 
-setClass("CertificateManager",
-    representation(location="character", obscuredpath="character")
+#' CertficateManager provides mechanisms to obtain, load, verify, and display X509 certificates.
+#' @description CertficateManager provides management functions for X.509 certificates that are 
+#' used to authenticate connections to DataONE nodes over SSL.  The X.509 certificates are issued 
+#' by a recognized Certificate Authority, typically CILogon, and include fields that provide 
+#' information about the authenticated party, including the distinguished name of the subject, the 
+#' dates of validity of the certificate, and other information needed for authorization decisions.  
+#' Certificate validity is determined by examining the validity of the certificate signatures for 
+#' each certificate in a chain leading to a trusted root certificate.  Within DataONE, the current 
+#' trusted root certificate authorities are CILogon and DataONE itself.
+#' 
+#' @details   
+#' Understanding how your identity is managed is important for working with DataONE, especially to 
+#' avoid unexpected results. For example, depending your authorization status, searches may or may 
+#' return only public records, or the full set of public and private records. Object and package 
+#' retrievals might fail if some or all of the objects being retrieved are private.  Creating or 
+#' updating objects on DataONE nodes and reserving identifiers reservations might fail if your 
+#' authorization certificate is missing or expired.
+#' 
+#' DataONE identifies you using CILogon-provided x509 certificates. DataONE has 
+#' partnered with CILogon to provide a widely-accessible certificate issuing mechanism 
+#' that allows DataONE users to use existing trusted institutional and public accounts.
+#' 
+#' CILogon recognizes many identity providers, including many universities as well as
+#' Google, so most times users new to DataONE can get certificates using one
+#' of their existing accounts. For more information about the CILogon service, see 
+#' \url{https://cilogon.org/?skin=DataONE} .
+#' 
+#' X509 Certificates differ from typical username-password login schemes in that
+#' certificates can be used by more than one application, which is very useful when
+#' using more than one DataONE-enabled application.  The certificates CILogon issues
+#' for DataONE are so-called "short-lived" certificates that currently expire 18 hours 
+#' from the time of issuing.  Typically you will want to download a fresh certificate
+#' the first time you interact with DataONE each day.
+#' 
+#' @slot location value of type \code{"character"}, containing a path to a custom certificate location
+#' @slot obscuredpath value of type \code{"character"}, containing the path used to temporarily obscure a certificate
+#' @author Matthew Jones, Rob Nahf
+#' @rdname CertificateManager-class
+#' @keywords classes
+#' @examples
+#' cm <- CertificateManager()
+#' cert <- getCertLocation(cm)
+#' s <- showClientSubject(cm)
+#' expires <- getCertExpires(cm)
+#' isExpired <- isCertExpired(cm)
+#' cm <- obscureCert(cm)
+#' cm <- restoreCert(cm)
+setClass("CertificateManager", slots = c(
+    location="character", 
+    obscuredpath="character"
+    )
 )
 
+#' Create a CertificateManager object
+#' @description Construct an instance of CertficateManager to provide mechanisms to obtain, load, verify, and 
+#' display X509 certificates.  If the \code{'location'} field is provided, then that location is interpreted
+#' as the fully qualified path to a certificate on the local filesystem, and the default locations will not be
+#' searched.  If \code{'location'} is missing, then the default Globus Grid Security Infrastructure (GSI) 
+#' location is searched, which is \code{'/tmp/x509up_u${UID}'} on Unix 
+#' or \code{'${tmpdir}/x509up_u${UID}'} on Windows or \code{'${tmpdir}/x509up_u${user.name}'} if \code{'${UID}'} 
+#' is not defined.
+#' @return the CertificateManager object
+#' @export
 setGeneric("CertificateManager", function(...) {
     standardGeneric("CertificateManager")
 })
 
+#' @describeIn CertificateManager
 setMethod("CertificateManager", , function() {
    result <- new("CertificateManager")
    result@location=as.character(NA)
@@ -34,22 +93,20 @@ setMethod("CertificateManager", , function() {
    return(result)
 })
 
-## Get DataONE Identity as Stored in the CILogon Certificate
-## 
-## Returns Your Identity according to DataONE (and CILogon).  If the certificate
-## is expired, the character string will be prefixed with "[EXPIRED]"
-## @returnType character
-## @return the DataONE Subject that is your client's identity
-## 
-## @author Matt Jones
-## @export
-
+#' Get DataONE Identity as Stored in the CILogon Certificate.
+#' @description Returns Your Identity according to DataONE (and CILogon) as provided in the Subject
+#' field of the X.509 certificate.  The value is a Distinguished Name, and can be used in all fields that
+#' require a user identity for access control authorization. If the certificate is missing on expired, then
+#' the subject 'public' is returned.
+#' @param x a CertificateManager instance
+#' @return the DataONE Subject that is your client's identity
+#' @export
 setGeneric("showClientSubject", function(x, ...) { 
-            standardGeneric("showClientSubject")
-        })
+    standardGeneric("showClientSubject")
+})
 
+#' @describeIn CertificateManager
 setMethod("showClientSubject", signature("CertificateManager"), function(x) {
-    
     PUBLIC="public"
     certfile <- getCertLocation(x)
     if (!is.null(certfile)) {
@@ -65,18 +122,19 @@ setMethod("showClientSubject", signature("CertificateManager"), function(x) {
     return(subject)
 })
 
-## Is the CILogon Certificate Expired?
-## @returnType logical
-## @return true if expired
-## 
-## @author Matt Jones
-## @export
-
-
+#' Determine if an X.509 certificate has expired.
+#' @description Returns \code{'TRUE'} if the certificate associated with a CertificateManager instance is expired.
+#' A certificate is expired if any of the following conditions hold: 1) the current time is before or after the 
+#' certificate validity dates, 2) the certificate is not valid according to a trusted Certificate Authority, or
+#' 3) no certificate can be found.
+#' @param x a CertificateManager instance
+#' @return TRUE if the certificate is expired
+#' @export
 setGeneric("isCertExpired", function(x, ...) { 
             standardGeneric("isCertExpired")
         })
 
+#' @describeIn CertificateManager
 setMethod("isCertExpired", signature("CertificateManager"), function(x) {
     expires <- getCertExpires(x)
     if (is.null(expires)) {
@@ -91,17 +149,17 @@ setMethod("isCertExpired", signature("CertificateManager"), function(x) {
     }
 })
 
-## Show the Date and Time when the CILogon Certificate Expires
-## @returnType character
-## @return the expiration date
-## 
-## @author Matt Jones
-## @export
-
+#' Show the date and time when an X.509 certificate expires.
+#' @description Each X.509 has a range of certificate validity times.  This method returns the X.509 
+#' \code{'notAfter'} field formatted as a \code{'POSIXct'} date value.
+#' @param x a CertificateManager instance
+#' @return POSIXct value
+#' @export
 setGeneric("getCertExpires", function(x, ...) { 
             standardGeneric("getCertExpires")
         })
 
+#' @describeIn CertificateManager
 setMethod("getCertExpires", signature("CertificateManager"), function(x) {
     certfile <- getCertLocation(x)
     if (!is.null(certfile)) {
@@ -113,42 +171,41 @@ setMethod("getCertExpires", signature("CertificateManager"), function(x) {
     return(expires)
 })
 
-
-## open the CILogin Certificate download page in the default browser
-## 
-## A convenience method to take you to the CILogon download page:  
-## https://cilogon.org/?skin=DataONE
-## @returnType NULL
-## 
-## @author rnahf
-## @export
+#' Open the CILogon Certificate download page in the default browser.
+#' @description A convenience method to take you to the CILogon download page:  
+#' \url{'https://cilogon.org/?skin=DataONE'}. Logging into CILogon will allow
+#' you to download your X.509 certificate to your local computer.  Typically,
+#' the certificate is saved in the default Globus location for certificates 
+#' (\code{\link{showCertLocation}}) and once it is there, the \code{'dataone'} 
+#' package will use the certificate for all authenticated operations.  Deleting 
+#' the certificate file is the equivalent of logging out.
+#' @param x a CertificateManager instance
+#' @return NULL
+#' @export
 setGeneric("downloadCert", function(x, ...) { 
-            standardGeneric("downloadCert")
-        })
+    standardGeneric("downloadCert")
+})
 
+#' @describeIn CertificateManager
 setMethod("downloadCert", signature("CertificateManager"), function(x) {
     browseURL("https://cilogon.org/?skin=DataONE")
 })
 
-
-
-## Obscure the CILogon Client Certificate
-## 
-## Obscures the x509 certificate that CILogon installs, effectively making future
-## interactions with the DataONE services anonymous.  Note, when the client
-## certificate is obscured, you will not be able to create objects to DataONE,
-## or build D1Objects, which uses the certificate to fill out fields in the
-## system metadata.
-## 
-## @note \code{restoreCert} is this method's inverse operation   
-## @returnType NULL
-## 
-## @author Matt Jones
-## @export
+#' Obscure the CILogon Client Certificate
+#' @description Obscures the x509 certificate that CILogon installs, effectively making future
+#' interactions with the DataONE services public/anonymous.  This function simple renames an
+#' existing certificate file to a known location, allowing 'public' operations.  Note, when the 
+#' client certificate is obscured via the renaming, you will not be able to create objects in 
+#' DataONE, or utilize any other methods that require authentication.
+#' @param x a CertificateManager instance
+#' @return the modified CertificateManager instance
+#' @seealso \code{\link{restoreCert}} is this method's inverse operation   
+#' @export
 setGeneric("obscureCert", function(x, ...) { 
-            standardGeneric("obscureCert")
-        })
+    standardGeneric("obscureCert")
+})
 
+#' @describeIn CertificateManager
 setMethod("obscureCert", signature("CertificateManager"), function(x) {
     certpath <- getCertLocation(x)
     if (!is.null(certpath)) {
@@ -158,22 +215,21 @@ setMethod("obscureCert", signature("CertificateManager"), function(x) {
     return(x)
 })
 
-
-## Restore an Obscured Certificate
-## 
-## Restores an obscured certificate to its original location. The inverse
-## operation to "obscureCert".  
-## 
-## @returnType NULL
-## 
-## @author Matt Jones
-## @export
+#' Restore the CILogon client certificate by renaming it to its original location
+#' @description Restores the x509 certificate that CILogon installs, which allows future
+#' interactions with nodes to be authenticated with the certificate.  This function simply 
+#' renames an obscured certificate file to its original location, allowing authenticated 
+#' operations.
+#' @param x a CertificateManager instance
+#' @return the modified CertificateManager instance
+#' @seealso \code{\link{obscureCert}} is this method's inverse operation   
+#' @export
 setGeneric("restoreCert", function(x, ...) { 
-            standardGeneric("restoreCert")
-        })
+    standardGeneric("restoreCert")
+})
 
+#' @describeIn CertificateManager
 setMethod("restoreCert", signature("CertificateManager"), function(x) {
-
     certpath <- getCertLocation(x)
     if (!is.null(certpath)) {
         ## if we got here, a new certificate is in the default location, so
@@ -188,22 +244,28 @@ setMethod("restoreCert", signature("CertificateManager"), function(x) {
         if (file.exists(x@obscuredpath)) {
             file.rename(x@obscuredpath, expectedLoc)
         } else {
-            message("No obscured certificate to restore at", x@obscuredpath)
+            message("No obscured certificate to restore at ", x@obscuredpath)
         }
         x@obscuredpath <- as.character(NA)
     }
+    return(x)
 })
 
-## Get the location on disk of the client certificate file
-## 
-## @returnType character
-## 
-## @author Matt Jones
-## @export
+#' Get the fiel path on disk of the client certificate file.
+#' @description Find the location of the client certificate, which is typically in a default
+#' location on disk, unless the \code{'location'} slot has been set with a custom location for
+#' the certificate.
+#' @details The default  Globus Grid Security Infrastructure (GSI) location is \code{'/tmp/x509up_u${UID}'} 
+#' on Unix or \code{'${tmpdir}/x509up_u${UID}'} on Windows or \code{'${tmpdir}/x509up_u${user.name}'} 
+#' if \code{'${UID}'} is not defined.
+#' @param x a CertificateManager instance
+#' @return character the path to the certificate
+#' @export
 setGeneric("getCertLocation", function(x, ...) { 
     standardGeneric("getCertLocation")
 })
 
+#' @describeIn CertificateManager
 setMethod("getCertLocation", signature("CertificateManager"), function(x) {
     
     # default Globus Grid Security Infrastructure (GSI) location, which is /tmp/x509up_u${UID} on Unix 
