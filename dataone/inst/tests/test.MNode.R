5f993f641c813d7fdb92209c7a62b2e3c4364b2c
Matt Jones <gitcode@magisa.org>
2015-02-10 14:57:39 -0900

    Remove extraneous library calls in test.

--- dataone/inst/tests/test.MNode.R
+++ dataone/inst/tests/test.MNode.R
@@ -66,10 +66,7 @@ test_that("MNode describe()", {
 })
 test_that("MNode create(), update(), archive(), and delete()", {
     skip_on_cran()
-    library(datapackage)
     library(dataone)
-    library(digest)
-    library(httr)
     cn <- CNode("STAGING2")
     mn <- getMNode(cn, "urn:node:mnTestKNB")
     newid <- generateIdentifier(mn, "UUID")
