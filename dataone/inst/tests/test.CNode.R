0e9be8c4114523a3210e9809d83fa882001efb5b
Peter Slaughter <slaughter@nceas.ucsb.edu>
2015-10-03 20:29:41 -0700

    Use qualified name dataone::describe in test, so it doesn't get overridden by thatthat::describe

--- dataone/inst/tests/test.CNode.R
+++ dataone/inst/tests/test.CNode.R
@@ -57,7 +57,7 @@ test_that("CNode describe()", {
   library(dataone)
   cn <- CNode("STAGING2")
   pid <- "aceasdata.3.2"
-  res <- describe(cn, pid)
+  res <- dataone::describe(cn, pid)
   expect_is(res, "list")
   expect_that(res$`content-type`, matches("text/xml"))
 })
