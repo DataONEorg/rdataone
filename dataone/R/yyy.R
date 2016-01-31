252c1aab4a9084fd2de0208e992ad44fd4c0a89e
Peter Slaughter <slaughter@Peters-MBP.domain>
2016-01-31 11:52:02 -0800

    Add docs for defunct method

---
+++ dataone/R/yyy.R
@@ -0,0 +1,10 @@
+#' @title Defunct
+#' @description The following items are defunct in this release of dataone and are
+#' no longer supported.
+#' @name dataone-defunct
+#' @keywords internal
+#' @section These S4 methods are defunct:
+#' \itemize{
+#'  \item{\code{\link{setObsoletedBy}}}{: Set a pid as being obsoleted by another pid.}
+#' }
+NULL
