a1236bde5a93220890602ec58077a1755ae24e05
Peter Slaughter <slaughter@Peters-MBP.domain>
2016-02-04 20:33:06 -0800

    Remove obsolete files

--- rst2html.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script to generate a HTML version of the README.txt file
-# Using a stylesheet similar to that used by the 
-# architecture docs
-
-curl "https://repository.dataone.org/software/tools/trunk/docutils/css/dataone.css" > /tmp/dataone.css
-rst2html.py --stylesheet /tmp/dataone.css README.rst > README.html
-
