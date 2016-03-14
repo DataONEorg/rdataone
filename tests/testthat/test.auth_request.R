4a2e3eca6a66a668e78d41a1e17ed146a11bc5e5
sbpcs59 <slaughter.nceas.ucsb.edu>
2016-03-14 05:40:15 -0700

    Use v2 endpoint in unit test

--- tests/testthat/test.auth_request.R
+++ tests/testthat/test.auth_request.R
@@ -6,7 +6,6 @@ test_that("dataone library loads", {
 test_that("auth_get", {
   library(dataone)
   library(httr)
-  uri <- "https://cn.dataone.org/cn/v1/formats"
   format_list <- xmlParse(content(dataone:::auth_get(uri)))
   cname <- class(format_list)[1]
   expect_that(cname, matches("XML"))
