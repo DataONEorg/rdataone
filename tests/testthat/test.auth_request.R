4b9aa6ae4abee69d87f11aa2d4c612c1e1844b80
Matt Jones <gitcode@magisa.org>
2016-08-31 16:38:45 -0800

    Fix auth_get test to properly pass in a node reference.

--- tests/testthat/test.auth_request.R
+++ tests/testthat/test.auth_request.R
@@ -8,7 +8,7 @@ test_that("auth_get", {
   library(httr)
   library(XML)
   uri <- "https://cn.dataone.org/cn/v2/formats"
-  format_list <- xmlParse(content(dataone:::auth_get(uri), as="text"))
+  format_list <- xmlParse(content(dataone:::auth_get(uri,node=CNode()), as="text"))
   cname <- class(format_list)[1]
   expect_that(cname, matches("XML"))
   xml <- XML::saveXML(format_list)
