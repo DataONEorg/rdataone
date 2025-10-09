b20efa81639a37bc4204ae4bdc79b76049a57979
Matt Jones <gitcode@magisa.org>
2025-10-08 18:05:30 -0800

    Change test dependency to httr, remove `curl::`

--- tests/testthat/test.A_curl_verision.R
+++ tests/testthat/test.A_curl_verision.R
@@ -1,5 +1,5 @@
 test_that("curl supports TLS1.3", {
-    library(dataone)
-    ssl_version <- curl::curl_version()$ssl_version
+    library(httr)
+    ssl_version <- curl_version()$ssl_version
     expect_match(ssl_version, "OpenSSL|LibreSSL|Schannel")
 })
