236f5e6cd6a53456f2678044e96a7a77dff429d8
Matt Jones <gitcode@magisa.org>
2025-10-08 18:30:06 -0800

    user curl package for ssl_version check.

--- tests/testthat/test.A_curl_verision.R
+++ tests/testthat/test.A_curl_verision.R
@@ -1,5 +1,4 @@
 test_that("curl supports TLS1.3", {
-    library(httr)
-    ssl_version <- curl_version()$ssl_version
+    ssl_version <- curl::curl_version()$ssl_version
     expect_match(ssl_version, "OpenSSL|LibreSSL|Schannel")
 })
