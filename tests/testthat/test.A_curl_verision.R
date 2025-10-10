4e6a6d44522239aee75f40ac62213c16f7d69c8e
Matt Jones <gitcode@magisa.org>
2025-10-10 15:47:18 -0800

    Add tests against SANDBOX CN server running TLSv1.3

--- tests/testthat/test.A_curl_verision.R
+++
@@ -1,4 +0,0 @@
-test_that("curl supports TLS1.3", {
-    ssl_version <- curl::curl_version()$ssl_version
-    expect_match(ssl_version, "OpenSSL|LibreSSL|Schannel")
-})
---
+++ tests/testthat/test.ssl.R
@@ -0,0 +1,27 @@
+test_that("curl SSL library expected", {
+    ssl_version <- curl::curl_version()$ssl_version
+    expect_match(ssl_version, "OpenSSL|LibreSSL|Schannel")
+})
+
+test_that("CNode constructors work in TLSv1.3 sandbox server", {
+    skip_on_cran()
+    library(dataone)
+    cnSandbox <- CNode("SANDBOX")
+    expect_match(cnSandbox@endpoint, "https://cn-sandbox.test.dataone.org/cn")
+})
+
+test_that("CNode listNodes() works in TLSv1.3 sandbox server", {
+    skip_on_cran()
+    library(dataone)
+    cnSandbox <- CNode("SANDBOX")
+    nodelist <- listNodes(cnSandbox)
+    expect_true(length(nodelist) > 0)
+})
+
+test_that("D1Client constructors work in TLSv1.3 sandbox server", {
+    skip_on_cran()
+    cli <- D1Client("SANDBOX")
+    expect_false(is.null(cli))
+    expect_match(class(cli), "D1Client")
+    expect_match(cli@cn@baseURL, "https://cn-sandbox.test.dataone.org/cn")
+})
